eab5fc04de522b7bb5a238e5b89911b48513f3cc
fashion <fashion@openbfdev.com>
2026-03-27 05:08:51 +0800

    fix init: remove useless code

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,8 +6,6 @@ WIFI_PASSWD=${PASSWD-12345678}
 
 nmcli c add type wifi con-name ${WIFI_SSID} ifname wlan0 ssid ${WIFI_SSID} wifi-sec.key-mgmt wpa-psk wifi-sec.psk ${WIFI_PASSWD} connection.autoconnect yes
 
-nmcli c down 3dprinter
-
 systemctl disable hotspot
 
 systemctl enable usb_host
